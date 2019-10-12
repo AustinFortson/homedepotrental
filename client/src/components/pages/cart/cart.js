7c31ca03cc542363d5a9f86d4be5cdfc1c92b98f
Austin <a.rossfortson@gmail.com>
2019-10-11 20:17:03 -0400

    updated cart

--- client/src/components/pages/cart/cart.js
+++ client/src/components/pages/cart/cart.js
@@ -13,10 +13,31 @@ class Cart extends React.Component {
             this.props.tools.map(Tools=> {
         return (
                 <div className="cart-tool">
-                <img src={Tools.img} className="cart-tool-img"/>
-                <h3>{Tools.name}</h3>
-                <h4>Deposit: ${Tools.deposit}</h4>
-                <h5>Quantity: {Tools.quantity}</h5>
+                    <Row>
+                        <Col xs={6} md={4} lg={3}>
+                            <img src={Tools.img} className="cart-tool-img"/>
+                        </Col>
+                        <Col xs={6} md={8} lg={9}>
+                            <h3>{Tools.name}</h3>
+                            <h4>Deposit: ${Tools.deposit}</h4>
+                            <h5>Quantity: {Tools.quantity}<a id="addIcon">+</a><a id="subtractIcon">-</a></h5>
+                            <a>Remove</a>
+                        </Col>
+                    </Row>
+                    <Row id="cart-prices">
+                        <Col xs={3}>
+                         <p>4 Hours: ${Tools.four_hour}</p>    
+                        </Col>
+                        <Col xs={3}>
+                         <p>24 Hours: ${Tools.daily}</p>    
+                        </Col>
+                        <Col xs={3}>
+                         <p>48 Hours: ${Tools.daily * 2}</p>    
+                        </Col>
+                        <Col xs={3}>
+                         <p>Week: ${Tools.weekly}</p>    
+                        </Col>
+                    </Row>
                 </div>
         );
         })
